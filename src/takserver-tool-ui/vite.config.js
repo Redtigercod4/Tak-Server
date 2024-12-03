9f66a812a0f52bf2cc793f8bb8f2952f3ec557dc
Redtigercod4 <76014409+Redtigercod4@users.noreply.github.com>
2024-12-03 12:57:50 +0000

    Moving build command into vite config

--- src/takserver-tool-ui/vite.config.js
+++ src/takserver-tool-ui/vite.config.js
@@ -1,11 +1,27 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
     plugins: [react()],
     build: {
         outDir: "./build",
-        emptyOutDir: true
+        emptyOutDir: true,
+        rollupOptions: {
+            input: path.resolve(__dirname, 'index.html'),
+            output: {
+                entryFileNames: 'static/js/main.js',
+                chunkFileNames: 'static/js/main.js',
+                assetFileNames: ({ names }) => {
+                    const filename = Array.isArray(names) ? names[names.length - 1] : names;
+
+                    if (filename && filename.endsWith('.css')) {
+                        return 'static/css/main.min.css';
+                    }
+                    return 'static/[name]-[hash][extname]';
+                }
+            }
+        }
     }
 });
